f1f19fc870776332bbd6d5d1bfba005aaf4d905f
ilyass bourass <ilyass.b.1992@gmail.com>
2025-11-18 14:54:07 +0100

    Ajout de UserDetailsService et configuration de l'authentification avec BCrypt

--- src/main/java/com/alamane/security/controller/UserController.java
+++ src/main/java/com/alamane/security/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.alamane.security.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/test")
 public class UserController {
+    @GetMapping
+    public String test(){
+        return "test";
+    }
+
 }
