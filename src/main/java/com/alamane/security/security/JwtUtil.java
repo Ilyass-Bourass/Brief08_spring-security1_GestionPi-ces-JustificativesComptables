2b84c94a1c0ede57d672ff9f2912ff6ca390ba78
ilyass bourass <ilyass.b.1992@gmail.com>
2025-11-19 11:40:25 +0100

    Ajout de JwtUtil pour la génération et validation des tokens JWT

---
+++ src/main/java/com/alamane/security/security/JwtUtil.java
@@ -0,0 +1,64 @@
+package com.alamane.security.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Component
+public class JwtUtil {
+    @Value("${app.jwt.secret}")
+    private String secret;
+
+    @Value("${app.jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String email) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims,email);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().
+                setClaims(claims).
+                setIssuedAt(new Date()).
+                setExpiration(new Date(System.currentTimeMillis()+expiration)).
+                signWith(getSigningKey(), SignatureAlgorithm.HS256).
+                compact();
+    }
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private boolean validateToken(String token, UserDetails userDetails) {
+        String email = extractEmail(token);
+        return (email.equals(userDetails.getUsername()) && !isTokenExpired(token));
+
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+}
